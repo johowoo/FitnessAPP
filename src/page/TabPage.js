42f192dde36ffee11eedbc914e38dcaf28ed2636
johowoo <316488922@qq.com>
2019-05-02 14:36:45 +1200

    change tab bar to icon

--- src/page/TabPage.js
+++ src/page/TabPage.js
@@ -1,45 +1,59 @@
 import React, {Component} from 'react';
 import {
-    StyleSheet, View, Text, Dimensions,
+    StyleSheet, View, Text, Dimensions, ScrollView
 } from 'react-native';
-
 import LinearGradient from 'react-native-linear-gradient';
 import TabView from 'react-native-scrollable-tab-view';
 import {Calendar} from './Calendar';
+import {Health} from './Health';
 import {CurrentWorkout} from '../component';
-import {Health} from "./Health";
+import {CustomBar} from '../component/CustomBar';
+
+// import {Progress} from "./Progress";
 
 class TabPage extends Component {
     render() {
         return (
             <LinearGradient
-
-                // colors={["#0077fe", "#00fe81"]}
-
                 colors={["#1b98d9", "#57c5b8"]}
                 style={{flex: 1}}
             >
                 <TabView
                     tabBarTextStyle={{fontSize: 25}}
                     tabBarPosition="bottom"
-                    tabBarUnderlineStyle={{backgroundColor:"#787"}}
+                    tabBarUnderlineStyle={{backgroundColor: "#787"}}
                     tabBarActiveTextColor="#787"
                     tabBarInactiveTextColor="#ddd"
+                    renderTabBar={() => <CustomBar/>}
                 >
-                    <CurrentWorkout tabLabel="+"></CurrentWorkout>
-
-                    <View tabLabel="Calendar">
-                        <Calendar/>
-                    </View>
-
-                    <View tabLabel="Health">
-                        <Health/>
-                    </View>
+                    <ScrollView tabLabel="ios-fitness">
+                        <View>
+                            <CurrentWorkout/>
+                        </View>
+                    </ScrollView>
+                    <ScrollView tabLabel="ios-paper">
+                        <View>
+                            <Calendar/>
+                        </View>
+                    </ScrollView>
+                    <ScrollView tabLabel="ios-trending-up">
+                        <View>
+                            <Health/>
+                        </View>
+                    </ScrollView>
                 </TabView>
-
             </LinearGradient>
         )
     }
 }
 
 export default TabPage;
+
+const styles = StyleSheet.create({
+    tabView: {
+
+    },
+    card: {
+
+    },
+});
