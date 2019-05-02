bbc20c6f3fbb6e168993381b03dec1ce9202aca3
AlecZeng <zxh93321@gmail.com>
2019-05-03 10:09:43 +1200

    image picker

--- src/page/Progress.js
+++ src/page/Progress.js
@@ -1,29 +1,14 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Dimensions, Button, TouchableOpacity, ScrollView} from 'react-native';
-import {Fonts} from "../utils/Fonts";
-import {
-    LineChart,
-} from 'react-native-chart-kit'
+import {View, Text, Image, Dimensions, TouchableOpacity, ScrollView, StyleSheet} from 'react-native';
+import {Fonts} from '../utils/Fonts';
 import Icon from "react-native-vector-icons/MaterialIcons";
-import {AddDropdown} from '../component/AddDropdown';
-import {connect} from 'react-redux';
-import {updateBfrAction, updateWeightAction} from '../store/actions'
 import ImagePicker from 'react-native-image-picker';
 import {TopBar} from "../component";
+import {connect} from 'react-redux'
+import {updateBfrAction, updateWeightAction} from "../store/actions";
 
 const {width, height} = Dimensions.get('window');
 
-const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-const chartConfig = {
-    backgroundGradientFrom: '#1b98d9',
-    backgroundGradientTo: '#666',
-    decimalPlaces: 2, // optional, defaults to 2dp
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    style: {
-        borderRadius: 16
-    }
-}
 
 const options = {
     title: 'Select Avatar',
@@ -40,6 +25,7 @@ export class _Progress extends Component {
         this.state = {
             isAddingWeight: false,
             isAddingBFR: false,
+            photo: null
         }
     }
 
@@ -60,7 +46,9 @@ export class _Progress extends Component {
                             <TouchableOpacity style={{height: 25, width: 25, backgroundColor: 'transparent'}}
                                               onPress={() => ImagePicker.showImagePicker(options, (response) => {
                                                   console.log('Response = ', response);
-
+                                                  if (response.uri) {
+                                                      this.setState({photo: response})
+                                                  }
                                                   if (response.didCancel) {
                                                       console.log('User cancelled image picker');
                                                   } else if (response.error) {
@@ -85,7 +73,9 @@ export class _Progress extends Component {
                                    key="add"/></TouchableOpacity>
                         </View>
                     </View>
-                    <View style={{height: height * 0.53}}></View>
+                    <View>
+                        {this.state.photo && <Image style={styles.image} source={{uri: this.state.photo.uri}}/>}
+                    </View>
                 </ScrollView>
 
             </View>
@@ -124,49 +114,12 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: '#fff',
         textAlign: 'center',
-        // marginLeft: 80
-
-    }
-})
-
-const dropdownStyles = StyleSheet.create({
-    container: {
-        height: 80,
-        marginBottom: 10,
-        marginTop: 10,
     },
-    dropdownInput: {
-        backgroundColor: "transparent",
-        height: 40,
-        borderColor: 'rgba(255,140,0,0.8)',
-        borderWidth: 1,
-        borderRadius: 10,
-        color: '#ddd'
-    },
-    dropdownContainer: {
-        padding: width * 0.03,
-        paddingTop: width * 0.02,
-    },
-    dropdownMenu: {
-        height: 35,
-        borderColor: 'rgba(255,140,0,0.8)',
-    },
-    dropdownMenuText: {
-        fontSize: 18,
-        color: '#FF8c00',
-    },
-    dropdownList: {
-        marginTop: 10
-    },
-    dropdownListText: {
-        fontSize: 18,
-        color: '#FF8c00',
-    },
-    dropdownSelection: {
-        color: "#00cccc"
-    },
-    confirmButton: {
-        height: 35,
-        borderColor: "rgba(255,140,0,0.8)"
+    image: {
+        flex: 1,
+        alignItems:'center',
+        justifyContent:'center',
+        marginTop: 50,
+        height: height * 0.6
     }
 });
