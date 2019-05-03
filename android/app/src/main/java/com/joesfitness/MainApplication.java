29ac5ba1b893970d409329b579e512dc1559e325
johowoo <316488922@qq.com>
2019-05-03 22:10:20 +1200

    fix bugs of camera

--- android/app/src/main/java/com/joesfitness/MainApplication.java
+++ android/app/src/main/java/com/joesfitness/MainApplication.java
@@ -3,7 +3,6 @@ package com.joesfitness;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.reactnative.camera.RNCameraPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNCameraPackage(),
             new SvgPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage()
