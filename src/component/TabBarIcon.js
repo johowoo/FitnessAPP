b8135d80cdcc2f73c3a26d5cb9cad7a3dfca56ef
johowoo <316488922@qq.com>
2019-05-09 22:37:27 +1200

    reconstruct the project by using react-navigation to replace react-native-scrollable-tabl-view

---
+++ src/component/TabBarIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Icon} from 'expo';
+
+export class TabBarIcon extends React.Component {
+    render() {
+        return (
+            <Icon.Ionicons
+                name={this.props.name}
+                size={26}
+                style={{marginBottom: -3, backgroundColor: "transparent"}}
+                color={this.props.focused ? "#c69" : "#666"}
+            />
+        );
+    }
+}
--- src/page/TabPage.js
+++ src/page/TabPage.js
@@ -1,53 +1,103 @@
 import React, {Component} from 'react';
 import {
-    StyleSheet, View, Text, Dimensions, ScrollView
+    StyleSheet, View, ScrollView
 } from 'react-native';
+
 import {LinearGradient} from 'expo';
 import TabView from 'react-native-scrollable-tab-view';
 import {Calendar} from './Calendar';
 import {Health} from './Health';
 import {CurrentWorkout} from '../component';
 import {CustomBar} from '../component/CustomBar';
-
+import {createBottomTabNavigator, createAppContainer} from 'react-navigation'
 import {Progress} from "./Progress";
+import {TabBarIcon} from '../component/TabBarIcon'
+
+let fontLoaded = true;
+
+const BottomTabNavigator = createBottomTabNavigator({
+    CurrentWorkout: {
+        screen: (xxx) => {
+            console.warn(xxx);
+            return <CurrentWorkout fontLoaded={fontLoaded}/>
+        },
+        navigationOptions: () => ({
+            tabBarIcon: ({focused}) => <TabBarIcon name={"ios-fitness"} focused={focused}/>,
+            tabBarLabel: 'fitness'
+        }),
+    },
+    Calendar: {
+        screen: () => <Calendar fontLoaded={fontLoaded}/>,
+        navigationOptions: () => ({
+            tabBarIcon: ({focused}) => <TabBarIcon name={"ios-calendar"} focused={focused}/>,
+            tabBarLabel: 'calendar'
+        }),
+    },
+    Health: {
+        screen: () => <Health fontLoaded={fontLoaded}/>,
+        navigationOptions: () => ({
+            tabBarIcon: ({focused}) => <TabBarIcon name={"ios-trending-up"} focused={focused}/>,
+            tabBarLabel: "health"
+        }),
+    },
+    Progress: {
+        screen: () => <Progress fontLoaded={fontLoaded}/>,
+        navigationOptions: () => ({
+            tabBarIcon: ({focused}) => <TabBarIcon name={"ios-images"} focused={focused}/>,
+            tabBarLabel: "progress"
+        }),
+    }
+}, {
+    tabBarOptions: {
+        activeTintColor: '#c69',
+        inactiveBackgroundColor: "#57c5b8",
+        style: {
+            backgroundColor: "#57c5b8"
+        }
+    }
+})
+
+const BottomTabContainer = createAppContainer(BottomTabNavigator);
 
 class TabPage extends Component {
     render() {
+        fontLoaded = this.props.fontLoaded;
         return (
             <LinearGradient
                 colors={["#1b98d9", "#57c5b8"]}
                 style={{flex: 1}}
             >
-                <TabView
-                    tabBarTextStyle={{fontSize: 25}}
-                    tabBarPosition="bottom"
-                    tabBarUnderlineStyle={{backgroundColor: "#787"}}
-                    tabBarActiveTextColor="#787"
-                    tabBarInactiveTextColor="#ddd"
-                    renderTabBar={() => <CustomBar/>}
-                    style={{marginBottom: 15}}
-                >
-                    <ScrollView tabLabel="ios-fitness">
-                        <View>
-                            <CurrentWorkout fontLoaded={this.props.fontLoaded}/>
-                        </View>
-                    </ScrollView>
-                    <ScrollView tabLabel="ios-calendar">
-                        <View>
-                            <Calendar fontLoaded={this.props.fontLoaded}/>
-                        </View>
-                    </ScrollView>
-                    <ScrollView tabLabel="ios-trending-up">
-                        <View>
-                            <Health fontLoaded={this.props.fontLoaded}/>
-                        </View>
-                    </ScrollView>
-                    <ScrollView tabLabel="ios-images">
-                        <View>
-                            <Progress fontLoaded={this.props.fontLoaded}/>
-                        </View>
-                    </ScrollView>
-                </TabView>
+                <BottomTabContainer fontLoaded={this.props.fontLoaded}/>
+                {/*    <TabView*/}
+                {/*        tabBarTextStyle={{fontSize: 25}}*/}
+                {/*        tabBarPosition="bottom"*/}
+                {/*        tabBarUnderlineStyle={{backgroundColor: "#787"}}*/}
+                {/*        tabBarActiveTextColor="#787"*/}
+                {/*        tabBarInactiveTextColor="#ddd"*/}
+                {/*        renderTabBar={() => <CustomBar/>}*/}
+                {/*        style={{marginBottom: 15}}*/}
+                {/*    >*/}
+                {/*        <ScrollView tabLabel="ios-fitness">*/}
+                {/*            <View>*/}
+                {/*                <CurrentWorkout fontLoaded={this.props.fontLoaded}/>*/}
+                {/*            </View>*/}
+                {/*        </ScrollView>*/}
+                {/*        <ScrollView tabLabel="ios-calendar">*/}
+                {/*            <View>*/}
+                {/*                <Calendar fontLoaded={this.props.fontLoaded}/>*/}
+                {/*            </View>*/}
+                {/*        </ScrollView>*/}
+                {/*        <ScrollView tabLabel="ios-trending-up">*/}
+                {/*            <View>*/}
+                {/*                <Health fontLoaded={this.props.fontLoaded}/>*/}
+                {/*            </View>*/}
+                {/*        </ScrollView>*/}
+                {/*        <ScrollView tabLabel="ios-images">*/}
+                {/*            <View>*/}
+                {/*                <Progress fontLoaded={this.props.fontLoaded}/>*/}
+                {/*            </View>*/}
+                {/*        </ScrollView>*/}
+                {/*    </TabView>*/}
             </LinearGradient>
         )
     }
