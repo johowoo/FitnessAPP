2c87134dd2fb010293f6eb789efdce3665f2069d
johowoo <316488922@qq.com>
2019-05-10 15:54:03 +1200

    update displayPicture

--- src/page/DisplayPicture.js
+++ src/page/DisplayPicture.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, Dimensions, Button, TouchableOpacity, ScrollView, Image} from 'react-native';
-import {TopBar} from "../component";
 
 const {width, height} = Dimensions.get('window');
 import {LinearGradient} from 'expo';
@@ -25,8 +24,16 @@ export class _DisplayPicture extends Component {
                     // ref={(ref) => this.scrollView = ref}
                 >
                     <View style={styles.imageContainer}>
-                        <Text>{navProps.weight}</Text>
                         <Image style={styles.image} source={{uri: navProps?.photoURI}}/>
+                    </View>
+
+                    <View style={styles.textContainer}>
+                        <View style={styles.textInnerContainer}>
+                            <Text style={styles.text}>Weight:{navProps.weight}</Text>
+                        </View>
+                        <View style={styles.textInnerContainer}>
+                            <Text style={styles.text}>BFR: {navProps.BFR}</Text>
+                        </View>
                     </View>
                 </ScrollView>
             </LinearGradient>
@@ -54,11 +61,29 @@ const styles = StyleSheet.create({
         borderBottomColor: '#b0b0b0'
     },
     image: {
-        width: width * 0.9,
+        width: width * 0.85,
         height: height * 0.6
     },
     imageContainer: {
+        marginTop: 20,
         alignItems: "center",
         justifyContent: "center"
+    },
+    textContainer: {
+        marginTop: 10,
+        alignItems: "center",
+        justifyContent: 'center'
+    },
+    textInnerContainer: {
+        alignItems: "center",
+        justifyContent: 'center',
+        height: 50,
+        width: width * 0.8,
+        margin: 5,
+        backgroundColor: "rgba(0,100,100,0.2)"
+    },
+    text: {
+        color: "#eee",
+        fontFamily:'PattayaRegular'
     }
 })
