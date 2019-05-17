81a0f50aa112b425e642b9b32147bb276711434b
johowoo <316488922@qq.com>
2019-05-17 22:28:01 +1200

    update analysis

--- src/component/PeriodAnalysis.js
+++ src/component/PeriodAnalysis.js
@@ -1,14 +1,29 @@
 import React, {Component} from "react";
-import {View, Text, StyleSheet,} from "react-native";
+import {View, Text, StyleSheet, Dimensions,} from "react-native";
 import {AddDropdown} from "./AddDropdown";
+import ModalDropdown from "react-native-modal-dropdown";
+
+const {width} = Dimensions.get("window");
 
 export class PeriodAnalysis extends Component {
+    state = {
+        selectedCategory: "7days",
+    };
+
     handleConfirm = () => {
-    }
+
+    };
+
+    handleSelect = (index, value) => {
+        this.setState({selectedCategory: value});
+    };
 
     render() {
         return (
-            <View>
+            <View style={styles.wholeContainer}>
+                <Text style={styles.analysisTitle}>
+                    {`${this.state.selectedCategory} analysis`}
+                </Text>
                 <View>
                     <AddDropdown
                         handleConfirm={this.handleConfirm.bind(this)}
@@ -20,15 +35,53 @@ export class PeriodAnalysis extends Component {
                             "6 month",
                             "year"
                         ]}
-                        placeholder="  Please enter an exercise"
+                        hideTextInput={true}
+                        handleSelect={this.handleSelect.bind(this)}
                     />
                 </View>
-                <Text>
-                </Text>
+                <View style={{marginTop: -40}}>
+                    <View style={styles.textLine}>
+                        <Text style={styles.textTerm}>Sets:{}</Text>
+                        <Text style={styles.textTerm}>Reps:{}</Text>
+                    </View>
+                    <View style={styles.textLine}>
+                        <Text style={styles.textTerm}>Workouts:{}</Text>
+                        <Text style={styles.textTerm}>Volume:{}</Text>
+                    </View>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    wholeContainer: {
+        borderRadius:20,
+        borderWidth: 1,
+        borderColor:"#ccc",
+        margin:10
+    },
+    analysisTitle: {
+        color: "#ddd",
+        fontSize: 22,
+        height: 40,
+        // textAlign: "center",
+        margin: 20,
+        marginBottom: 0,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    textLine: {
+        flexDirection: "row",
+        height: 30
+    },
+    textTerm: {
+        fontSize: 18,
+        color: "#ddd",
+        width: width * 0.4,
+        marginLeft: width * 0.1
+    },
+});
 
 const dropdownStyles = StyleSheet.create({
     dropdownInput: {
