bd9c0380f319dbee5a43b90bda5f3004f8c3b857
johowoo <316488922@qq.com>
2019-05-18 10:37:23 +1200

    update UI

--- src/component/PeriodAnalysis.js
+++ src/component/PeriodAnalysis.js
@@ -2,12 +2,21 @@ import React, {Component} from "react";
 import {View, Text, StyleSheet, Dimensions,} from "react-native";
 import {AddDropdown} from "./AddDropdown";
 import ModalDropdown from "react-native-modal-dropdown";
+import {connect} from 'react-redux';
+import ApslButton from "apsl-react-native-button";
 
 const {width} = Dimensions.get("window");
 
-export class PeriodAnalysis extends Component {
+const NumberOfDaysArr = [1, 7, 30, 180, 365];
+
+class _PeriodAnalysis extends Component {
     state = {
-        selectedCategory: "7days",
+        selectedCategory: "",
+        selectedIndex: 0,
+        sets: 0,
+        reps: 0,
+        workouts: 0,
+        volume: 0
     };
 
     handleConfirm = () => {
@@ -15,45 +24,73 @@ export class PeriodAnalysis extends Component {
     };
 
     handleSelect = (index, value) => {
-        this.setState({selectedCategory: value});
+        this.setState({selectedCategory: value, selectedIndex: index});
+    };
+
+    handleConfirmPressed = () => {
+
     };
 
     render() {
         return (
             <View style={styles.wholeContainer}>
-                <Text style={styles.analysisTitle}>
-                    {`${this.state.selectedCategory} analysis`}
-                </Text>
-                <View>
-                    <AddDropdown
-                        handleConfirm={this.handleConfirm.bind(this)}
-                        styles={dropdownStyles}
-                        options={[
-                            "7 days",
-                            "Week",
-                            "month",
-                            "6 month",
-                            "year"
+                <View style={styles.dropdownContainer}>
+                    <ModalDropdown
+                        style={[styles.dropdownMenu]}
+                        textStyle={[
+                            styles.dropdownMenuText,
                         ]}
-                        hideTextInput={true}
-                        hideConfirmButton={true}
-                        handleSelect={this.handleSelect.bind(this)}
+                        dropdownStyle={[
+                            styles.dropdownList,
+                        ]}
+                        dropdownTextStyle={[
+                            styles.dropdownListText,
+                        ]}
+                        dropdownTextHighlightStyle={[
+                            styles.dropdownSelection,
+                        ]}
+                        options={[
+                            "24 Hour",
+                            "7 Days",
+                            "1 Month",
+                            "6 Months",
+                            "1 Year"
+                        ]}
+                        defaultValue={"24 Hour"}
+                        onSelect={this.handleSelect.bind(this)}
+                    />
+                    <Text style={styles.analysisTitle}>analysis</Text>
+                    <ApslButton
+                        style={[styles.confirmButton]}
+                        onPress={this.handlePress}
+                        children={
+                            <Text key="confirm" style={{color: "#FF8c00",fontSize:16}}>
+                                Confirm
+                            </Text>
+                        }
                     />
                 </View>
                 <View style={{marginTop: -40}}>
                     <View style={styles.textLine}>
-                        <Text style={styles.textTerm}>Sets:{}</Text>
-                        <Text style={styles.textTerm}>Reps:{}</Text>
+                        <Text style={styles.textTerm}>Sets:{this.state.sets}</Text>
+                        <Text style={styles.textTerm}>Reps:{this.state.reps}</Text>
                     </View>
                     <View style={styles.textLine}>
-                        <Text style={styles.textTerm}>Workouts:{}</Text>
-                        <Text style={styles.textTerm}>Volume:{}</Text>
+                        <Text style={styles.textTerm}>Workouts:{this.state.workouts}</Text>
+                        <Text style={styles.textTerm}>Volume:{this.state.volume}</Text>
                     </View>
                 </View>
             </View>
         )
     }
 }
+
+const mapStateToProps = state => ({
+    markedDates: state.calendar.markedDates,
+    allExercisesList: state.savedExerciseForEachDay.allExercisesList,
+});
+
+export const PeriodAnalysis = connect(mapStateToProps, null)(_PeriodAnalysis);
 
 const styles = StyleSheet.create({
     wholeContainer: {
@@ -65,9 +102,11 @@ const styles = StyleSheet.create({
     analysisTitle: {
         color: "#ddd",
         fontSize: 22,
-        height: 40,
+        height: 30,
         // textAlign: "center",
-        margin: 20,
+        margin: 10,
+        marginLeft: 0,
+        // marginTop:5,
         marginBottom: 0,
         alignItems: "center",
         justifyContent: "center"
@@ -81,6 +120,46 @@ const styles = StyleSheet.create({
         color: "#ddd",
         width: width * 0.4,
         marginLeft: width * 0.1
+    },
+    dropdownContainer: {
+        height: 110,
+        padding: width * 0.03,
+        paddingTop: width * 0.02,
+        flexDirection: "row",
+    },
+    dropdownMenu: {
+        width: width *0.28,
+        height: 40,
+        borderRadius: 6,
+        marginRight: width * 0.02,
+        borderWidth: 1,
+        borderColor: "#FF8c00",
+        // backgroundColor:'#EEE',
+        justifyContent: "center",
+    },
+    dropdownMenuText: {
+        marginLeft: 10,
+        fontSize: 18,
+        color: "#FF8c00",
+    },
+    dropdownList: {
+        width: width *0.28,
+        marginTop: 15,
+    },
+    dropdownListText: {
+        fontSize: 18,
+        marginLeft: 10,
+        color: "#FF8c00",
+    },
+    dropdownSelection: {
+        color: "#00cccc",
+    },
+    confirmButton: {
+        backgroundColor:"rgba(200,200,200,0.1)",
+        marginLeft: 36,
+        height: 40,
+        width: width * 0.25,
+        borderColor: "#FF8c00",
     },
 });
 
