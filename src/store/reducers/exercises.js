1d6d140ad2dbd6045c38708e1c825971adeb6af7
johowoo <316488922@qq.com>
2019-05-20 13:23:55 +1200

    update delete algorithm

--- src/store/reducers/exercises.js
+++ src/store/reducers/exercises.js
@@ -6,27 +6,29 @@ import * as types from "../actionTypes";
 
 export const exercises = (
     state = {sectionExercises, extraSectionExercises}, action
-) => {
+    ) => {
 
-    switch (action.type) {
-        case types.ADD_EXERCISE_TO_SECTIONLIST:
-            const cacheExercises = JSON.parse(JSON.stringify(state.sectionExercises));
-            cacheExercises.map(value => {
-                if (value.category === action.payload.category) {
-                    value.data.push(action.payload.item);
-                }
-            });
-            return {...state, sectionExercises: cacheExercises};
-        case types.DELETE_EXERCISE_FROM_SECTIONLIST:
-            const cacheExercisesDelete = JSON.parse(JSON.stringify(state.sectionExercises));
-            cacheExercisesDelete.map((item, index) => {
-                if (item.data.includes(action.payload)) {
-                    console.warn(item.data);
-                    item.data.splice(item.data.indexOf(item), 1);
-                }
-            });
-            return  {...state, sectionExercises: cacheExercisesDelete};
-        default:
-            return state;
+        switch (action.type) {
+            case types.ADD_EXERCISE_TO_SECTIONLIST:
+                const cacheExercises = JSON.parse(JSON.stringify(state.sectionExercises));
+                cacheExercises.map(value => {
+                    if (value.category === action.payload.category) {
+                        value.data.push(action.payload.item);
+                    }
+                });
+                return {...state, sectionExercises: cacheExercises};
+            case types.DELETE_EXERCISE_FROM_SECTIONLIST:
+                const cacheExercisesDelete = [];
+                state.sectionExercises.map((item, index) => {
+                        const emptyElement = {};
+                        emptyElement.category = item.category;
+                        emptyElement.data = item.data.filter((i) => i !== action.payload);
+                        cacheExercisesDelete.push(emptyElement);
+                    }
+                );
+            return {...state, sectionExercises: cacheExercisesDelete};
+            default:
+                return state;
+        }
     }
-};
+;
