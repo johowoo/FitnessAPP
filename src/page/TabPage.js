f84feab17db9541cc6800d498ea1f6f4c4b59ca1
johowoo <316488922@qq.com>
2019-05-23 11:56:18 +1200

    update

--- src/page/TabPage.js
+++ src/page/TabPage.js
@@ -22,56 +22,56 @@ import {formatYYYY_MM_DD_HHMMFromParams} from '../utils/formatMonthandDay';
 let fontLoaded = true;
 
 const StackNavigator = createStackNavigator({
-    Progress: {
-        screen: props => <Progress fontLoaded={fontLoaded} {...props} />,
-        navigationOptions: {
-            header: null,
+        Progress: {
+            screen: props => <Progress fontLoaded={fontLoaded} {...props} />,
+            navigationOptions: {
+                header: null,
+            },
         },
-    },
-    DisplayPicture: {
-        screen: props => <DisplayPicture {...props}  />,
-        screenProps: "",
-        navigationOptions: ({navigation}) => ({
-            gesturesEnabled: false,
-            // headerTitle: navigation?.state?.params?.date.toString()
-            headerTitle: formatYYYY_MM_DD_HHMMFromParams(navigation?.state?.params),
-            // `${navigation?.state?.params?.year.toString()}-${navigation?.state?.params?.month.toString()}-${navigation?.state?.params?.day.toString()} ${navigation?.state?.params?.hour.toString()}:${navigation?.state?.params?.minute.toString()}`,
-            // header: null
-            headerBackTitle: "Progress Page",
-            headerRight: (
-                <TouchableOpacity style={{marginRight: 30}}
-                                  onPress={() => {
-                                      navigation?.state?.params?.showDeleteConfirmModalInDisplayPicture({
-                                          showReminder: true,
-                                          reminderTitle: 'Delete',
-                                          reminderContent: "Do you want to delete this photo？",
-                                          hideConfirmButton: false
-                                      });
-                                      // Alert.alert("Delete", "Do you want to delete this photo？", [
-                                      //     {
-                                      //         text: "Delete",
-                                      //         onPress: async () => {
-                                      //             await LoadingUtil.showLoading();
-                                      //             await navigation?.state?.params?.deleteOnePicFromProgress({});
-                                      //             await LoadingUtil.dismissLoading();
-                                      //         },
-                                      //     },
-                                      //     {text: "Cancel"},
-                                      // ])
-                                  }
-                                  }>
-                    <View>
-                        <Icon name="trash-o" size={24} color="#c69" key="delete"/>
-                    </View>
-                </TouchableOpacity>
-            ),
-            // headerLeftContainerStyle: {color: "#fff"},
-            // headerTintColor: {color: "#FFF"},
-            headerBackground: (
-                <LinearGradient colors={["#1b98d9", "#219dd5"]} style={{flex: 1}}/>
-            ),
-            headerTintColor: "#c69",
-            // headerTitleStyle: {
+        DisplayPicture: {
+            screen: props => <DisplayPicture {...props}  />,
+            screenProps: "",
+            navigationOptions: ({navigation}) => ({
+                gesturesEnabled: false,
+                // headerTitle: navigation?.state?.params?.date.toString()
+                headerTitle: formatYYYY_MM_DD_HHMMFromParams(navigation?.state?.params),
+                // `${navigation?.state?.params?.year.toString()}-${navigation?.state?.params?.month.toString()}-${navigation?.state?.params?.day.toString()} ${navigation?.state?.params?.hour.toString()}:${navigation?.state?.params?.minute.toString()}`,
+                // header: null
+                headerBackTitle: "Progress Page",
+                headerRight: (
+                    <TouchableOpacity style={{marginRight: 30}}
+                                      onPress={() => {
+                                          navigation?.state?.params?.showDeleteConfirmModalInDisplayPicture({
+                                              showReminder: true,
+                                              reminderTitle: 'Delete',
+                                              reminderContent: "Do you want to delete this photo？",
+                                              hideConfirmButton: false
+                                          });
+                                          // Alert.alert("Delete", "Do you want to delete this photo？", [
+                                          //     {
+                                          //         text: "Delete",
+                                          //         onPress: async () => {
+                                          //             await LoadingUtil.showLoading();
+                                          //             await navigation?.state?.params?.deleteOnePicFromProgress({});
+                                          //             await LoadingUtil.dismissLoading();
+                                          //         },
+                                          //     },
+                                          //     {text: "Cancel"},
+                                          // ])
+                                      }
+                                      }>
+                        <View>
+                            <Icon name="trash-o" size={24} color="#c69" key="delete"/>
+                        </View>
+                    </TouchableOpacity>
+                ),
+                // headerLeftContainerStyle: {color: "#fff"},
+                // headerTintColor: {color: "#FFF"},
+                headerBackground: (
+                    <LinearGradient colors={["#1b98d9", "#219dd5"]} style={{flex: 1}}/>
+                ),
+                headerTintColor: "#c69",
+                // headerTitleStyle: {
             //     color: "#eee",
             // },
             // headerBackTitleStyle: {
@@ -81,7 +81,9 @@ const StackNavigator = createStackNavigator({
             // }
         }),
     },
-});
+    }
+)
+;
 const CurrentWorkoutStackNavigator = createStackNavigator({
     CurrentWorkout: {
         screen: props => <CurrentWorkout fontLoaded={fontLoaded} {...props} />,
@@ -105,8 +107,9 @@ const CurrentWorkoutStackNavigator = createStackNavigator({
             ),
             headerTintColor: "#c69",
             headerTitleStyle: {
+                marginLeft: 30,
                 color: "rgba(204,102,153,0.85)",
-                fontSize: 22,
+                fontSize: 25,
                 fontFamily: "PattayaRegular"
             },
             // headerBackTitleStyle: {
@@ -134,8 +137,9 @@ const CurrentWorkoutStackNavigator = createStackNavigator({
             ),
             headerTintColor: "#c69",
             headerTitleStyle: {
+                marginLeft: 30,
                 color: "rgba(204,102,153,0.85)",
-                fontSize: 24,
+                fontSize: 25,
                 fontFamily: "PattayaRegular"
             },
             // headerBackTitleStyle: {
