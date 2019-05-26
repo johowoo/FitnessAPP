5bf63b327e63c7c9e4c2b14a97240d51e7e46b59
johowoo <316488922@qq.com>
2019-05-26 12:22:01 +1200

    fix

--- src/component/EditWeightReps.js
+++ src/component/EditWeightReps.js
@@ -101,7 +101,7 @@ export class EditWeightReps extends Component {
                                 options={[
                                     "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "15", "20", "25", "30"
                                 ]}
-                                defaultValue={this.props.sets}
+                                defaultValue={this.props.sets.toString()}
                                 onSelect={this.handleSelect.bind(this)}
                             /><Text style={{color: "#666", fontSize: 16}}>sets</Text>
                         </View>
