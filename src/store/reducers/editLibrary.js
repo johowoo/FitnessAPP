f061159d4f1775c0d354998fa929db46a3b3e6b9
johowoo <316488922@qq.com>
2019-05-28 22:28:14 +1200

    update

--- src/store/reducers/editLibrary.js
+++ src/store/reducers/editLibrary.js
@@ -6,7 +6,6 @@ export const editLibrary = (
         showAddWeightModalForEditLibrary: false,
         showEditWeightRepsForEditLibrary: false,
         showAddCategoryModal: false,
-        workoutHistoryExerciseList: {}
     },
     action
 ) => {
@@ -39,23 +38,6 @@ export const editLibrary = (
             return {
                 ...state,
             };
-        case types.ADD_EXERCISES_TO_EXERCISE_LIST_OF_WORKOUT_HISTORY:
-            if (!action.payload.weightRepsDataArr) {
-                action.payload.weightRepsDataArr = [];
-            }
-            const currentDateExerciseListCopy = state.workoutHistoryExerciseList[action.payload.date] ? JSON.parse(JSON.stringify(state.workoutHistoryExerciseList[action.payload.date])) : [];
-            currentDateExerciseListCopy.push(action.payload);
-            // const WorkHistoryExerciseCopy = JSON.parse(JSON.stringify(state.workoutHistoryExerciseList));
-            // return [...state, action.payload];
-            return {
-                ...state,
-                workoutHistoryExerciseList: {
-                    ...state.workoutHistoryExerciseList,
-                    [action.payload.date]: currentDateExerciseListCopy
-                }
-            };
-        case types.ADD_EXERCISES_LIST_TO_WORKOUT_HISTORY:
-            return {...state};
         default:
             return state;
     }
